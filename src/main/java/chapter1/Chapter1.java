7982ddab166b4962bc76fd5b8d82c87e8d6bcbe3
Aaron Ploetz <aaron.ploetz@target.com>
2024-12-07 13:25:29 -0600

    adding problems to chapter1

---
+++ src/main/java/chapter1/Chapter1.java
@@ -0,0 +1,72 @@
+package chapter1;
+
+public class Chapter1 {
+
+	public static void main(String[] args) {
+		
+		Integer[] numbers = {1, 2, 3, 4, 5};
+		printArray(numbers);
+		
+		Integer[] reversed = reverseArray(numbers);
+		printArray(reversed);
+		
+		printDiamond(5);
+	}
+
+	private static void printArray(Integer[] array) {
+		
+		for (Integer number: array) {
+			System.out.println(number);
+		}
+		System.out.println();
+	}
+	
+	private static Integer[] reverseArray(Integer[] array) {
+		
+		Integer[] reversedArray = new Integer[array.length];
+		int reversedIndex = 0;
+		
+		for (int arrayIndex = array.length; arrayIndex > 0; arrayIndex--) {
+			reversedArray[reversedIndex] = array[arrayIndex - 1];
+			reversedIndex++;
+		}
+		
+		return reversedArray;
+	}
+	
+	private static void printDiamond(int size) {
+		
+		// upper half
+		for (int topCounter = 1; topCounter <= size; topCounter++) {
+			// compute and print leading spaces
+			for (int spaceCounter = 0; spaceCounter < (size - topCounter); spaceCounter++) {
+				System.out.print(" ");
+			}
+			// print number(s)
+			if (topCounter > 1) {
+				// filler spaces, not needed for the first iteration
+				System.out.print(topCounter);
+				for (int fillCounter = 0; fillCounter < ((topCounter * 2) - 3); fillCounter++) {
+					System.out.print(" ");
+				}
+			}
+			System.out.println(topCounter);
+		}
+		// bottom half
+		for (int bottomCounter = (size - 1); bottomCounter > 0; bottomCounter--) {
+			// compute and print leading spaces
+			for (int spaceCounter2 = 0; spaceCounter2 < (size - bottomCounter); spaceCounter2++) {
+				System.out.print(" ");
+			}
+			// print number(s)
+			if (bottomCounter > 1) {
+				// filler spaces, not needed for the first iteration
+				System.out.print(bottomCounter);
+				for (int fillCounter2 = 0; fillCounter2 < ((bottomCounter * 2) - 3); fillCounter2++) {
+					System.out.print(" ");
+				}
+			}
+			System.out.println(bottomCounter);
+		}
+	}
+}
