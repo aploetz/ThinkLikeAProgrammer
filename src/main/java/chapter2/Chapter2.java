5862019845b5000a5c992b152842bb7116efc652
Aaron Ploetz <aaron.ploetz@target.com>
2024-12-26 08:59:19 -0600

    bcrypt exercises for chapter 2

--- src/main/java/chapter2/Chapter2.java
+++ src/main/java/chapter2/Chapter2.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class Chapter2 {
 
 	public static void main(String[] args) {
@@ -18,8 +20,10 @@ public class Chapter2 {
 				"Pitfall!", "Frogger"
 				).toList();
 		writeListToFile(dataFile1, atariGames);
-		
+		System.out.println();
+
 		readTextFile(dataFile1);
+		System.out.println();
 		
 		String csvFile1 = "arcadeScores.csv";
 		List<String> arcadeScores = Stream.of(
@@ -29,8 +33,22 @@ public class Chapter2 {
 				"Super Mario Brothers,Coriene,239400"
 				).toList();
 		writeListToFile(csvFile1, arcadeScores);
+		System.out.println();
 		
 		readCSVFile(csvFile1);
+		System.out.println();
+		
+		writeToPasswordFile("aaron", "Brewers82");
+		writeToPasswordFile("coriene", "loveBug22");
+		writeToPasswordFile("avery", "rumbleBoy14");
+		writeToPasswordFile("christian", "BHappy42");
+		writeToPasswordFile("emily", "chimPoKoMon58");
+		writeToPasswordFile("victoria", "ethanIsAwesome77");
+		
+		validateUserFromFile("aaron","Brewers82");
+		validateUserFromFile("avery","blahblahblah");
+		System.out.println();
+		
 	}
 	
 	private static void writeListToFile(String filename, List<String> list) {
@@ -99,6 +117,66 @@ public class Chapter2 {
 		} catch (IOException ioex) {
 			System.out.println("Error occurred while reading:");
 			ioex.printStackTrace();
+		}
+	}
+	
+	private static void writeToPasswordFile(String username, String password) {
+		try {
+			// open file with append == true
+			BufferedWriter writer = new BufferedWriter(new FileWriter("data/password.csv",true));
+
+	    	// Hash the password from the request body
+	    	BCryptPasswordEncoder pEncoder = new BCryptPasswordEncoder();
+	    	String hashedPassword = pEncoder.encode(password);
+			
+			writer.write(username + "," + hashedPassword + "\n");
+
+			writer.close();
+		} catch (IOException ioex) {
+			System.out.println("Error occurred while writing:");
+			ioex.printStackTrace();
+		}
+
+		System.out.println(username + " written to password file.");
+	}
+
+	private static void validateUserFromFile(String username, String password) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("data/password.csv"));
+			
+	    	BCryptPasswordEncoder pEncoder = new BCryptPasswordEncoder();
+	    	//String hashedPassword = pEncoder.encode(password);
+	    	boolean found = false;
+	    	
+	    	String line = reader.readLine();
+	    	
+	    	while (line != null) {
+		    	String[] columns = line.split("[,]");
+	    		String fileUser = columns[0];
+	    		
+	    		if (fileUser.equals(username)) {
+	    			found = true;
+	    			String fileHashedPwd = columns[1];
+	    			
+	    			//if (fileHashedPwd.equals(hashedPassword)) {
+	    			if (pEncoder.matches(password, fileHashedPwd)) {
+	    				System.out.printf("User %s has valid credentials.\n", username);
+	    			} else {
+	    				System.out.printf("User %s could not be authenticated.\n", username);
+	    			}
+	    			break;
+	    		}
+	    		line = reader.readLine();
+	    	}
+	    	
+	    	if (!found) {
+	    		System.out.printf("User %s was not found.\n", username);
+	    	}
+	    	
+			reader.close();
+		} catch (IOException ioex) {
+			System.out.println("Error occurred while reading:");
+			ioex.printStackTrace();			
 		}
 	}
 }
