04e86d2b3b579d66fdc620874c67bfe800dcd3de
Aaron Ploetz <aaron.ploetz@target.com>
2024-12-31 13:05:32 -0600

    fixing paste issuse in chapter 1, adding uppercase methods in chapter 3.

--- src/main/java/chapter3/Chapter3.java
+++ src/main/java/chapter3/Chapter3.java
@@ -7,6 +7,22 @@ public class Chapter3 {
 		String phrase = "Minnesota Wild Hockey";
 		System.out.println(reverseString(phrase));
 		
+		System.out.println();
+		
+		Character character1 = 'g';
+		System.out.printf("Is %c uppercase? %b\n", character1, isUppercaseReference(character1));
+		Character character2 = 'T';
+		System.out.printf("Is %c uppercase? %b\n", character2, isUppercaseReference(character2));
+
+		System.out.println();
+
+		char char1 = 'a';
+		System.out.printf("Is %c uppercase? %b\n", char1, isUppercasePrimitive(char1));
+		char char2 = 'P';
+		System.out.printf("Is %c uppercase? %b\n", char2, isUppercasePrimitive(char2));
+
+		System.out.println();
+		
 	}
 	
 	private static String reverseString(String inputStr) {
@@ -19,4 +35,21 @@ public class Chapter3 {
 		
 		return reversed.toString();
 	}
+	
+	private static boolean isUppercaseReference(Character inputChar) {
+		
+		return Character.isUpperCase(inputChar);
+	}
+
+	private static boolean isUppercasePrimitive(char inputChar) {
+		
+		int asciiCode = (int) inputChar;
+		
+		if (asciiCode > 64 && asciiCode < 91) {
+			return true;
+		}
+		
+		return false; 
+	}
+
 }
