ef0f37c759b398cee609ce92798f1e481a9b8b8d
Aaron Ploetz <aaron.ploetz@target.com>
2025-01-20 16:32:06 -0600

    prettying up code

--- src/main/java/chapter5/Chapter5.java
+++ src/main/java/chapter5/Chapter5.java
@@ -23,47 +23,28 @@ public class Chapter5 {
 
 		LinkedList directions = new LinkedList();
 		
-		UUID id1 = UUID.randomUUID();
-		Step step1 = new Step(id1, "After 0.1 miles, turn left onto Wuthering Hills Drive.");
+		Step step1 = new Step(UUID.randomUUID(), "After 0.1 miles, turn left onto Wuthering Hills Drive.");
+		Step step2 = new Step(UUID.randomUUID(), "After 1.3 miles, turn right onto Highway 11.");
+		Step step3 = new Step(UUID.randomUUID(), "After 0.3 miles, exit the roundabout onto the ramp for I-39S/I-90E.");
+		Step step4 = new Step(UUID.randomUUID(), "After 12 miles, take exit 1 for IL-75W/US-51N, and turn right.");
+		Step step5 = new Step(UUID.randomUUID(), "After 1.2 miles, turn right onto US-51N. You are now entering Wisconsin.");
+		Step step6 = new Step(UUID.randomUUID(), "After 1.1 miles, turn right onto Pleasant St.");
+		Step step7 = new Step(UUID.randomUUID(), "After 350 feet, turn left onto East Grand Ave.");
+		Step step8 = new Step(UUID.randomUUID(), "After 0.2 miles, turn right onto 2nd St.");
+		Step step9 = new Step(UUID.randomUUID(), "After 350 feet, you have arrived at 705 3rd St.");
+
 		directions.addStep(step1);
-		
-		UUID id2 = UUID.randomUUID();
-		Step step2 = new Step(id2, "After 1.3 miles, turn right onto Highway 11.");
 		directions.addStep(step2);
-		
-		UUID id3 = UUID.randomUUID();
-		Step step3 = new Step(id3, "After 0.3 miles, exit the roundabout onto the ramp for I-39S/I-90E.");
 		directions.addStep(step3);
-		
-		UUID id4 = UUID.randomUUID();
-		Step step4 = new Step(id4, "After 11 miles, you are now entering Illinois.");
 		directions.addStep(step4);
-		
-		UUID id5 = UUID.randomUUID();
-		Step step5 = new Step(id5, "After 1 mile, take exit 1 for IL-75W/US-51N, and turn right.");
 		directions.addStep(step5);
-		
-		UUID id6 = UUID.randomUUID();
-		Step step6 = new Step(id6, "After 1.2 miles, turn right onto US-51N. You are now entering Wisconsin.");
 		directions.addStep(step6);
-		
-		UUID id7 = UUID.randomUUID();
-		Step step7 = new Step(id7, "After 1.1 miles, turn right onto Pleasant St.");
 		directions.addStep(step7);
-		
-		UUID id8 = UUID.randomUUID();
-		Step step8 = new Step(id8, "After 350 feet, turn left onto East Grand Ave.");
 		directions.addStep(step8);
-		
-		UUID id9 = UUID.randomUUID();
-		Step step9 = new Step(id9, "After 0.2 miles, turn right onto 2nd St.");
 		directions.addStep(step9);
 		
-		UUID id10 = UUID.randomUUID();
-		Step step10 = new Step(id10, "After 350 feet, you have arrived at 705 3rd St. Beloit, WI.");
-		directions.addStep(step10);
-		
 		Step currentStep = step1;
+		
 		while (currentStep != null) {
 			System.out.printf("%s\n", currentStep.getDescription());
 			currentStep = currentStep.getNextStep();
