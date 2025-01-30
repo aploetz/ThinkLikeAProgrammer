e57d8e031d5be27b339886213edc2150276f69f1
Aaron Ploetz <aaron.ploetz@target.com>
2025-01-30 07:56:20 -0600

    added more phone number examples

--- src/main/java/chapter6/Chapter6.java
+++ src/main/java/chapter6/Chapter6.java
@@ -10,13 +10,19 @@ public class Chapter6 {
 		String phoneNumber2 = "(313)472-3929";
 		String phoneNumber3 = "313-459-7199";
 		String phoneNumber4 = "331234567";
+		String phoneNumber5 = "414.458.1556";
+		String phoneNumber6 = "44-458-3831";
+		String phoneNumber7 = "(414)882-113";
 		
-		Pattern phonePattern = Pattern.compile("[(]*[0-9]{3}[)-]*[0-9]{3}[-]*[0-9]{4}");
+		Pattern phonePattern = Pattern.compile("[(.]*[0-9]{3}[)-.]*[0-9]{3}[-.]*[0-9]{4}");
 
 		System.out.printf("is %s valid? %b\n", phoneNumber, isValidPhoneNumber(phoneNumber, phonePattern));
 		System.out.printf("is %s valid? %b\n", phoneNumber2, isValidPhoneNumber(phoneNumber2, phonePattern));
 		System.out.printf("is %s valid? %b\n", phoneNumber3, isValidPhoneNumber(phoneNumber3, phonePattern));
 		System.out.printf("is %s valid? %b\n", phoneNumber4, isValidPhoneNumber(phoneNumber4, phonePattern));
+		System.out.printf("is %s valid? %b\n", phoneNumber5, isValidPhoneNumber(phoneNumber5, phonePattern));
+		System.out.printf("is %s valid? %b\n", phoneNumber6, isValidPhoneNumber(phoneNumber6, phonePattern));
+		System.out.printf("is %s valid? %b\n", phoneNumber7, isValidPhoneNumber(phoneNumber7, phonePattern));
 
 	}
 	
